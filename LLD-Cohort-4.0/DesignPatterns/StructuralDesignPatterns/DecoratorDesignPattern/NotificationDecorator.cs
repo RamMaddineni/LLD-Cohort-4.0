238f59e6cea3bacb273e350cf254efd1334d6730
Ram Maddineni <maddineniramkumar29@gmail.com>
2026-09-10 07:09:33 +0530

    added abstract definitions in comments

--- LLD-Cohort-4.0/DesignPatterns/StructuralDesignPatterns/DecoratorDesignPattern/NotificationDecorator.cs
+++ LLD-Cohort-4.0/DesignPatterns/StructuralDesignPatterns/DecoratorDesignPattern/NotificationDecorator.cs
@@ -15,4 +15,9 @@ namespace LLD_Cohort_4._0.DesignPatterns.StructuralDesignPatterns.DecoratorDesig
         }
         public abstract void Send(string message);
     }
+
+/*    Abstract class: is a restricted class that cannot be used to create objects(to access it, it must be inherited from another class).
+
+Abstract method: can only be used in an abstract class, and it does not have a body.The body is provided by the derived class (inherited from).*/
+
 }
